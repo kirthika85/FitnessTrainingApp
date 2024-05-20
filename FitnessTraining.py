d8c366c8bfe0fb15e7041dc16b94e60941496795
kirthika85 <kirthika.rajamani@gmail.com>
2024-05-20 14:03:44 -0700

    Create FitnessTraining.py

---
+++ FitnessTraining.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+
+st.title("Fitness Training App")
+st.header("Enter Your Fitness Details")
+    
+goals = st.text_input("What are your fitness goals?")
+fitness_level = st.selectbox("What is your current fitness level?", ["Beginner", "Intermediate", "Advanced"])
+duration = st.slider("Duration of the training plan (in weeks)", 4, 8, 12)
+generate_button = st.button("Generate plan")
+
+if not openai_api_key.startswith('sk-'):
+   st.warning('Please enter your OpenAI API key!', icon='⚠')
+if generate_button and openai_api_key.startswith('sk-'):
+   if st.button("Generate Plan"):
+      if goals and fitness_level and duration:
+         llm=ChatOpenAI(api_key=openai_api_key,temperature=0.8,model_name="gpt-3.5-turbo")
+         prompt = (f"I am a {fitness_level.lower()} looking to achieve {goals} in {duration} weeks. "
+                   f"Can you provide me with a detailed weekly fitness training plan?")
+         response = llm.stream(prompt)
+         st.header("Your Fitness Plan")
+         st.text(response)
+      else:
+         st.error("Please provide all the required information.")
